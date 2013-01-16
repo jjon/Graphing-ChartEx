ba7fbccfdb03fe5321e51b01234952c34a66d414
Jon Crump <jjcrump@uw.edu>
2013-01-16 13:21:00 -0800

    made general enough to run from command line against any directory containing BRAT .ann files

--- ann2rdf.py
+++ ann2rdf.py
@@ -1,19 +1,15 @@
 #!/usr/local/bin/python2.7
 # -*- coding: utf-8 -*- #
+## Generates RDF for a directory full of .ann files
 
 from rdflib import Graph, Namespace, Literal, RDF, RDFS, OWL, XSD
 import re
 import os
+import sys
 
 
-g = Graph()
-g.bind("chartex", "http://yorkhci.org/chartex-schema#")
-g.bind("crm", "http://www.cidoc-crm.org/rdfs/cidoc-crm-english-label#")
-chartex = Namespace("http://yorkhci.org/chartex-schema#")
-crm = Namespace("http://www.cidoc-crm.org/rdfs/cidoc-crm-english-label#")
-
-
-def ann2rdf(annfile, ch_id):    
+def ann2rdf(g, annfile, ch_id):
+    """ Here is the crudest sort of parsing of the annotation file. Consider using brat's own annotation.py """
     this = Namespace("http://yorkhci.org/chartex-schema/"+ ch_id + "#")
     implied_siterefs = []
         
@@ -50,21 +46,62 @@ def addText(g, chfiles):
         g.add((sub, chartex.textData, Literal(text, datatype=XSD.string)))
 
 
-if __name__ == "__main__":
-    
-    ## get this from varargs for command-line execution
-    chfiles = "/Users/jjc/Sites/brat/data/chartex/deeds/"
-    
-    for f in os.listdir(chfiles):
-        annotationFile = ''
-        
-        f_path = os.path.join(chfiles,f)
-        ch_id, ext = os.path.splitext(f)
 
-        if ext == '.ann':
-            annotationFile = [line.replace('•','.') for line in open(f_path, "r").readlines()]
-            
-        ann2rdf(annotationFile, ch_id)
-        addText(g, chfiles)
+g = Graph()
+g.bind("chartex", "http://yorkhci.org/chartex-schema#")
+g.bind("crm", "http://www.cidoc-crm.org/rdfs/cidoc-crm-english-label#")
+chartex = Namespace("http://yorkhci.org/chartex-schema#")
+chartexDoc = Namespace("http://yorkhci.org/chartex-schema/")
+crm = Namespace("http://www.cidoc-crm.org/rdfs/cidoc-crm-english-label#")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) <= 1:
+        print >>sys.stderr, "Usage: python", sys.argv[0], "/path/to/directory/to/graph"
+    
+    elif not os.path.isdir(sys.argv[1]):
+        print >>sys.stderr, "directory %s unknown" % (sys.argv[1])
         
-    print g.serialize(format="xml")
+    elif os.path.isdir(sys.argv[1]):
+        #print list((f for f in os.listdir(sys.argv[1]) if os.path.splitext(f)[1] == ".ann"))
+#         for annfile in (f for f in os.listdir(sys.argv[1]) if os.path.splitext(f)[1] == ".ann"):
+#             ch_id, ext = os.path.splitext(annfile)
+#             ann2rdf(g, annfile, ch_id)
+#             addText(g, sys.argv[1])
+#             print g.serialize(format="n3")
+        chfiles = sys.argv[1]
+        for f in os.listdir(chfiles):
+            annotationFile = ''
+            
+            f_path = os.path.join(chfiles,f)
+            ch_id, ext = os.path.splitext(f)
+    
+            if ext == '.ann':
+                annotationFile = [line.replace('•','.') for line in open(f_path, "r").readlines()]
+                
+            ann2rdf(g, annotationFile, ch_id)
+            addText(g, chfiles)
+            
+        print g.serialize(format="n3")
+            
+            #>>sys.stderr, "directory %s contains no annotation files" % (sys.argv[1])
+    
+    else: print sys.argv
+    ## get this from varargs for command-line execution
+#     chfiles = "/Users/jjc/Sites/brat/data/chartex/deeds/"
+#     
+#     for f in os.listdir(chfiles):
+#         annotationFile = ''
+#         
+#         f_path = os.path.join(chfiles,f)
+#         ch_id, ext = os.path.splitext(f)
+# 
+#         if ext == '.ann':
+#             annotationFile = [line.replace('•','.') for line in open(f_path, "r").readlines()]
+#             
+#         ann2rdf(g, annotationFile, ch_id)
+#         addText(g, chfiles)
+#         
+#     print g.serialize(format="n3")
+#     
+## Consider another version that generates sub-graphs for each document, named-graphs, contexts, whatever, if we can work out how to construct useful sparql queries against them.
